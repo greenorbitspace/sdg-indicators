e58916fd7b8912bb3937398ffae1228926437630
rjmlaird <rjmlaird@users.noreply.github.com>
2025-07-30 02:17:21 +0000

    deploy: 9fe95206d59ba5897be9da517e34a16ceeaf5ddd

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sdg-indicators/service-worker.js');
+        navigator.serviceWorker.register('/service-worker.js');
     });
 }
 /**
